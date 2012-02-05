874c0a7dcaa0f7032f387341c02270b2884bf73f
dmrlook <dmrlook@hotmail.com>
2012-02-04 19:46:49 -0600

    Initial rev - identical to samsung opensource download

---
+++ SPH-M580_Kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Fri Dec 2 18:11:46 KST 2011"
+#define LINUX_COMPILE_TIME "18:11:46"
+#define LINUX_COMPILE_BY "acyther.kang"
+#define LINUX_COMPILE_HOST "NACDMA-BUILD-SVR3"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-51) "
